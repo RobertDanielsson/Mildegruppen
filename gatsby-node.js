cbbaa6dc77b3ad8a8775fa775636e2808f9b454c
robertdanielsson <robertdanielsson@live.com>
2021-01-21 23:32:10 +0100

    another fix in createpages

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,37 +4,37 @@ const slash = require("slash")
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
-  return graphql(
-    `
-      {
-        allContentfulTeam {
-          edges {
-            node {
-              slug
-            }
-          }
-        }
-      }
-    `
-  )
-    .then(result => {
-      if (result.errors) {
-        console.log("error with contentful data", result.errors)
-      }
+  // return graphql(
+  //   `
+  //     {
+  //       allContentfulTeam {
+  //         edges {
+  //           node {
+  //             slug
+  //           }
+  //         }
+  //       }
+  //     }
+  //   `
+  // )
+  //   .then(result => {
+  //     if (result.errors) {
+  //       console.log("error with contentful data", result.errors)
+  //     }
 
-      const teamTemplate = path.resolve("./src/templates/team.tsx")
+  //     const teamTemplate = path.resolve("./src/templates/team.tsx")
 
-      result.data.allContentfulTeam.edges.forEach(team => {
-        createPage({
-          path: `/teams/${team.node.slug}`,
-          component: slash(teamTemplate),
-          context: {
-            slug: team.node.slug,
-          },
-        })
-      })
-    })
-    .catch(error => {
-      console.log("Error with contentfl data", error)
-    })
+  //     result.data.allContentfulTeam.edges.forEach(team => {
+  //       createPage({
+  //         path: `/teams/${team.node.slug}`,
+  //         component: slash(teamTemplate),
+  //         context: {
+  //           slug: team.node.slug,
+  //         },
+  //       })
+  //     })
+  //   })
+  //   .catch(error => {
+  //     console.log("Error with contentfl data", error)
+  //   })
 }
